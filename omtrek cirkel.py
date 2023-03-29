903621dd6eb63009f2c0e9cbfbb4799772cf5849
Yannick Tuerlings <202259@student.aventus.nl>
2023-03-29 12:54:30 +0200

    cirkel verbetering

--- omtrek cirkel.py
+++ omtrek cirkel.py
@@ -1,14 +1,34 @@
 import math
+import tkinter as tk
 
-x = input("wat is de diameter?\n")
-x = int(x)
+# Functie om de berekeningen uit te voeren
+def berekenen():
+    x = int(diameter_entry.get())
+    omtrek = math.pi * x
+    straal = x / 2
+    oppervlakte = straal * 2 * math.pi
+    omtrek_label.config(text=f"De omtrek van uw cirkel is: {omtrek}")
+    oppervlakte_label.config(text=f"De oppervlakte van uw cirkel is: {oppervlakte}")
 
-omtrek = math.pi * x
-omtrek = str(omtrek)
+# Maak het basisvenster
+root = tk.Tk()
+root.title("Cirkelberekeningen")
 
-straal = x / 2
+# Maak het invoerveld voor de diameter
+diameter_label = tk.Label(root, text="Wat is de diameter?")
+diameter_label.pack()
+diameter_entry = tk.Entry(root)
+diameter_entry.pack()
 
-oppervlakte = straal * 2 * math.pi
-oppervlakte = str(oppervlakte)
+# Maak de knop om de berekeningen uit te voeren
+bereken_button = tk.Button(root, text="Bereken", command=berekenen)
+bereken_button.pack()
 
-print ("de omtrek van uw cirkel is: " + omtrek + "\n" "en de oppervlakte: " + oppervlakte)
+# Maak de labels voor de uitvoer
+omtrek_label = tk.Label(root, text="")
+omtrek_label.pack()
+oppervlakte_label = tk.Label(root, text="")
+oppervlakte_label.pack()
+
+# Start de GUI
+root.mainloop()
